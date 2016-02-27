e6cd7c08062f2e982ab72291ee8aeb958bcf9499
ellamiowgi <ladonne@gmail.com>
2016-02-26 23:59:48 -0500

    5

---
+++ class-13/scripts.js
@@ -0,0 +1,9 @@
+var listItems = document.getElementsByTagName('li');
+
+onListItemClick = function(e) {
+	alert("My beautiful ella is " + e.currentTarget.innerHTML);
+}
+
+for (var i = 0; i < listItems.length; i++) {
+	listItems[i].addEventListener('click', onListItemClick);
+}
